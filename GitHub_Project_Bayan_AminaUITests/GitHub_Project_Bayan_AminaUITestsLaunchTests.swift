f99a9da9ae261332007c4190f2783ac7f301b548
Bayan Buitek <bbayan@inbox.ru>
2021-11-24 18:02:48 +0600

    Initial Commit

---
+++ GitHub_Project_Bayan_AminaUITests/GitHub_Project_Bayan_AminaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitHub_Project_Bayan_AminaUITestsLaunchTests.swift
+//  GitHub_Project_Bayan_AminaUITests
+//
+//  Created by Buitek Bayan on 24.11.2021.
+//
+
+import XCTest
+
+class GitHub_Project_Bayan_AminaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
